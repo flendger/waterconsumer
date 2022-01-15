0f94f8f343dea128cd0a207aa333f42903fff9d8
flendger <kiru.ee@yandex.ru>
2022-01-15 22:13:20 +0300

    rename main class

--- src/main/java/ru/flendger/waterconsumer/WaterConsumerApplication.java
+++ src/main/java/ru/flendger/waterconsumer/WaterConsumerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WaterconsumerApplication {
+public class WaterConsumerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WaterconsumerApplication.class, args);
+		SpringApplication.run(WaterConsumerApplication.class, args);
 	}
 
 }
