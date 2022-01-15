8828f92ba0594351b744d6e53466962fd24df129
flendger <kiru.ee@yandex.ru>
2022-01-15 19:51:21 +0300

    Initial commit

---
+++ src/main/java/ru/flendger/waterconsumer/WaterconsumerApplication.java
@@ -0,0 +1,13 @@
+package ru.flendger.waterconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterconsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaterconsumerApplication.class, args);
+	}
+
+}
