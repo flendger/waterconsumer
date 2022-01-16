dfd8215128b75d1d9c595fb1d9f34ed8a1c4d89e
flendger <kiru.ee@yandex.ru>
2022-01-16 12:56:38 +0300

    added water_consumption entity

--- src/main/java/ru/flendger/waterconsumer/model/entity/WaterConsumption.java
+++ src/main/java/ru/flendger/waterconsumer/model/entity/WaterConsumption.java
@@ -1,20 +1,38 @@
 package ru.flendger.waterconsumer.model.entity;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "water_consumption")
 public class WaterConsumption {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+
+    @Column(name = "consumption_date")
     private LocalDate consumptionDate;
+
+    @Column(name = "quantity")
     private int quantity;
+
+    @Column(name = "created_at")
+    @CreationTimestamp
     private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    @UpdateTimestamp
     private LocalDateTime updatedAt;
 
     @Override
